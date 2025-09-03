846f430d0574370141cbd8b6906b1827dbc6b4df
윤동성 <ehdtjd0906@naver.com>
2025-09-03 21:05:03 +0900

    quotes 완료

--- src/app/quotes/QuotesClient.jsx
+++ src/app/quotes/QuotesClient.jsx
@@ -4,15 +4,19 @@ import React, { useState } from "react";
 import style from "./quotes.module.scss";
 import { IoReloadCircle } from "react-icons/io5";
 import axios from "axios";
+import Image from "next/image";
 
-const QuotesClient = ({ data }) => {
+const QuotesClient = ({ data, images, randomIndex }) => {
   const [quotes, setQuotes] = useState(data[0]);
+  const [imageIndex, setImageIndex] = useState(randomIndex);
 
   // 인용구 새로고침
   const reloadQuotes = async () => {
     try {
       const response = await axios.get("/api/quotes");
       const data = Array.isArray(response.data) ? response.data[0] : null;
+
+      setImageIndex(Math.floor(Math.random() * images.length));
 
       if (response) setQuotes(data);
     } catch (error) {
@@ -23,6 +27,14 @@ const QuotesClient = ({ data }) => {
   return (
     <div className="container">
       <div className={style["quotes-container"]}>
+        <Image
+          src={images[imageIndex]}
+          fill
+          priority
+          sizes="(max-width: 500px) 100vw, 500px"
+          className={style["bg"]}
+          alt="quotes_background"
+        />
         <div className={style["quotes-content"]}>
           <p>{quotes?.quote ?? ""}</p>
         </div>
--- src/app/quotes/page.jsx
+++ src/app/quotes/page.jsx
@@ -2,10 +2,21 @@ import React from "react";
 import QuotesClient from "./QuotesClient";
 import { getApiQuotes } from "@/lib/quotes";
 
+const BG_IMAGE = [
+  "/image/quotes_bg1.jpg",
+  "/image/quotes_bg2.jpg",
+  "/image/quotes_bg3.jpg",
+  "/image/quotes_bg4.jpg",
+  "/image/quotes_bg5.jpg",
+];
+
 const Quotes = async () => {
   const data = await getApiQuotes();
+  const randomIndex = Math.floor(Math.random() * BG_IMAGE.length);
 
-  return <QuotesClient data={data} />;
+  return (
+    <QuotesClient data={data} images={BG_IMAGE} randomIndex={randomIndex} />
+  );
 };
 
 export default Quotes;
