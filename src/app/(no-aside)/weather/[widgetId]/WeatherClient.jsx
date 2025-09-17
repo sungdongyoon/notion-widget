3cdd0d9f307d3d0b75dd7bd458044c55d75aea37
윤동성 <ehdtjd0906@naver.com>
2025-09-17 14:38:40 +0900

    weather 작업

--- src/app/(no-aside)/weather/[widgetId]/WeatherClient.jsx
+++ src/app/(no-aside)/weather/[widgetId]/WeatherClient.jsx
@@ -1,12 +1,38 @@
 "use client";
 
+import Image from "next/image";
 import React, { useEffect } from "react";
 
 const WeatherClient = ({ data, widgetId }) => {
+  // 날씨에 따른 이미지 맵
+  const WHEATHER_IMAGE_MAP = {
+    Clear: "sun",
+    Rain: "rainy",
+    Clouds: "cloud",
+  };
+
+  const weatherData = {
+    temp: Math.round(data.main.temp),
+    location: data.name,
+    image: WHEATHER_IMAGE_MAP[data.weather[0].main] ?? "sun",
+  };
+
   return (
     <div className="widget_container">
       <div className="weather_container" data-variant={widgetId}>
-        weather
+        <div className="weather_image">
+          <div className="weather_image_box">
+            <Image
+              src={`/test/${weatherData.image}.png`}
+              alt="sun image"
+              fill
+            />
+          </div>
+        </div>
+        <div className="weather_info">
+          <span className="weather_temp">{weatherData.temp}º</span>
+          <span className="weather_location">{weatherData.location}</span>
+        </div>
       </div>
     </div>
   );
--- src/lib/weather.jsx
+++ src/lib/weather.jsx
@@ -4,7 +4,7 @@ export async function getApiWeather() {
   const lat = "37.5511121532521";
   const lon = "126.98819670241075";
   const { data } = await axios.get(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.WEATHER_API_KEY}`
   );
 
   return data;
