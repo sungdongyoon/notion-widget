db530325b5c4acce1df3de79bffbb573fe713c4a
윤동성 <ehdtjd0906@naver.com>
2025-10-13 14:56:29 +0900

    timer02 스타일 변경 완료

--- src/components/widgets/timer/Timer02.jsx
+++ src/components/widgets/timer/Timer02.jsx
@@ -16,6 +16,8 @@ const Timer02 = () => {
   // 타이머 진행 여부
   const [running, setRunning] = useState(false);
 
+  const isInitial = !running && time === initialTime;
+
   // 시, 분, 초
   const hour = String(Math.floor(time / (1000 * 60 * 60))).padStart(2, "0");
   const minute = String(Math.floor((time / (1000 * 60)) % 60)).padStart(2, "0");
@@ -67,36 +69,34 @@ const Timer02 = () => {
     setTime(ms);
   };
 
-  console.log("time", time);
-
   return (
     <div className="widget_container" data-variant="timer02">
       <div className="bg-timer-02-bg relative max-w-[500px] min-w-[240px] w-full aspect-[1/1] flex flex-col items-center justify-between rounded-[50%]">
-        {/* <div className="absolute top-4 2xs:top-6 right-4 2xs:right-6">
-          <TimeOption value={time} onApply={applyTime} disabled={running} />
-        </div> */}
-
         <div className="w-full flex-[2] flex justify-center items-center relative">
-          <div className="absolute top-6 left-1/2 -translate-x-1/2 text-white">
-            <span>{hour}</span>:<span>{minute}</span>:<span>{second}</span>
-          </div>
+          {!isInitial && (
+            <div className="absolute top-[5%] left-1/2 -translate-x-1/2 text-[clamp(0.6rem,5vmin,1.2rem)] text-white">
+              <span>{hour}</span>:<span>{minute}</span>:<span>{second}</span>
+            </div>
+          )}
 
-          <div className="absolute bottom-6 left-1/2 -translate-x-1/2 flex gap-3">
-            <button
-              onClick={!running ? startTime : pauseTime}
-              aria-label={!running ? "시작" : "일시정지"}
-              className="inline-flex items-center justify-center text-white"
-            >
-              {!running ? (
-                <FaPlayCircle aria-hidden="true" />
-              ) : (
-                <FaPauseCircle aria-hidden="true" />
-              )}
-            </button>
-            <button className="inline-flex items-center justify-center text-white">
-              <FaArrowRotateLeft aria-hidden="true" />
-            </button>
-          </div>
+          {!isInitial && (
+            <div className="absolute bottom-[5%] left-1/2 -translate-x-1/2 flex gap-3">
+              <button
+                onClick={!running ? startTime : pauseTime}
+                aria-label={!running ? "시작" : "일시정지"}
+                className="inline-flex items-center justify-center text-[clamp(0.6rem,5vmin,1.2rem)] text-white"
+              >
+                {!running ? (
+                  <FaPlayCircle aria-hidden="true" />
+                ) : (
+                  <FaPauseCircle aria-hidden="true" />
+                )}
+              </button>
+              <button className="inline-flex items-center justify-center text-[clamp(0.6rem,5vmin,1.2rem)] text-white">
+                <FaArrowRotateLeft aria-hidden="true" />
+              </button>
+            </div>
+          )}
 
           <div
             className="timer_clock"
@@ -130,34 +130,6 @@ const Timer02 = () => {
                 </div>
               </div>
             )}
-          </div>
-        </div>
-
-        <div className="w-full flex-1 flex justify-center items-center fixed top-0 right-0">
-          <div className="bg-timer-02-timer-bg 2xs:max-w-[70%] max-w-[75%] w-full flex items-center justify-between 2xs:py-4 3xs:py-3 7xs:py-2 2xs:px-8 3xs:px-7 7xs:px-4 py-2 px-4 rounded-md shadow-xl">
-            <div className="timer_time">
-              <p className="text-timer-02-timer-text 2xs:text-[1.5rem] 5xs:text-[1.2rem] 6xs:text-[1rem] 7xs:text-[0.8rem] text-[0.8rem]">
-                <span>{hour}</span>:<span>{minute}</span>:<span>{second}</span>
-              </p>
-            </div>
-            <div className="timer_btn flex 5xs:gap-4 gap-2 items-center">
-              <button
-                onClick={startTime}
-                aria-label="시작"
-                disabled={running}
-                className="inline-flex items-center justify-center 2xs:text-[2rem] 6xs:text-[1.2rem] 7xs:text-[1rem] text-[0.8rem] text-timer-02-timer-text disabled:text-gray-500 disabled:cursor-not-allowed"
-              >
-                <FaPlayCircle aria-hidden="true" />
-              </button>
-              <button
-                onClick={pauseTime}
-                aria-label="시작"
-                disabled={!running}
-                className="inline-flex items-center justify-center 2xs:text-[2rem] 6xs:text-[1.2rem] 7xs:text-[1rem] text-[0.8rem] text-timer-02-timer-text disabled:text-gray-500 disabled:cursor-not-allowed"
-              >
-                <FaPauseCircle aria-hidden="true" />
-              </button>
-            </div>
           </div>
         </div>
       </div>
