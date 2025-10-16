9ade1012ed2ebd8d62f3f7f437acae08096bad64
윤동성 <ehdtjd0906@naver.com>
2025-10-16 23:43:06 +0900

    빌드 에러 fixed 테스트

--- src/app/(no-aside)/quotes/[widgetId]/page.tsx
+++ src/app/(no-aside)/quotes/[widgetId]/page.tsx
@@ -1,68 +1,76 @@
 import React from "react";
-import QuotesClient from "./QuotesClient";
-import { notFound } from "next/navigation";
-import { getApiQuotes } from "@/lib/quotes";
 
-// 이미지 리스트
-const BG_IMAGE = {
-  typeA: [
-    "/image/quotes/quotes_bg1.jpg",
-    "/image/quotes/quotes_bg2.jpg",
-    "/image/quotes/quotes_bg3.jpg",
-    "/image/quotes/quotes_bg4.jpg",
-    "/image/quotes/quotes_bg5.jpg",
-  ],
-  typeB: [
-    "/image/quotes/quotes_paper_bg1.png",
-    "/image/quotes/quotes_paper_bg2.png",
-    "/image/quotes/quotes_paper_bg3.png",
-    "/image/quotes/quotes_paper_bg4.png",
-    "/image/quotes/quotes_paper_bg5.png",
-    "/image/quotes/quotes_paper_bg6.png",
-    "/image/quotes/quotes_paper_bg7.png",
-    "/image/quotes/quotes_paper_bg8.png",
-  ],
-  typeC: [
-    "/image/quotes/quotes_paper_bg1.png",
-    "/image/quotes/quotes_paper_bg2.png",
-    "/image/quotes/quotes_paper_bg3.png",
-    "/image/quotes/quotes_paper_bg4.png",
-    "/image/quotes/quotes_paper_bg5.png",
-    "/image/quotes/quotes_paper_bg6.png",
-    "/image/quotes/quotes_paper_bg7.png",
-    "/image/quotes/quotes_paper_bg8.png",
-  ],
-} as const;
+const page = () => {
+  return <div>test</div>;
+};
 
-// 위젯 아이디 타입 가드
-const isWidgetId = (id: string): id is keyof typeof BG_IMAGE => id in BG_IMAGE;
+export default page;
 
-export default async function QuotesPage({
-  params,
-}: {
-  params: { widgetId: string };
-}) {
-  // 위젯 아이디
-  const widgetId = (params.widgetId ?? "").trim();
+// import React from "react";
+// import QuotesClient from "./QuotesClient";
+// import { notFound } from "next/navigation";
+// import { getApiQuotes } from "@/lib/quotes";
 
-  // 화이트 리스트에 걸리지 않는 위젯 아이디는 not found 페이지로 이동
-  if (!isWidgetId(widgetId)) {
-    notFound();
-  }
+// // 이미지 리스트
+// const BG_IMAGE = {
+//   typeA: [
+//     "/image/quotes/quotes_bg1.jpg",
+//     "/image/quotes/quotes_bg2.jpg",
+//     "/image/quotes/quotes_bg3.jpg",
+//     "/image/quotes/quotes_bg4.jpg",
+//     "/image/quotes/quotes_bg5.jpg",
+//   ],
+//   typeB: [
+//     "/image/quotes/quotes_paper_bg1.png",
+//     "/image/quotes/quotes_paper_bg2.png",
+//     "/image/quotes/quotes_paper_bg3.png",
+//     "/image/quotes/quotes_paper_bg4.png",
+//     "/image/quotes/quotes_paper_bg5.png",
+//     "/image/quotes/quotes_paper_bg6.png",
+//     "/image/quotes/quotes_paper_bg7.png",
+//     "/image/quotes/quotes_paper_bg8.png",
+//   ],
+//   typeC: [
+//     "/image/quotes/quotes_paper_bg1.png",
+//     "/image/quotes/quotes_paper_bg2.png",
+//     "/image/quotes/quotes_paper_bg3.png",
+//     "/image/quotes/quotes_paper_bg4.png",
+//     "/image/quotes/quotes_paper_bg5.png",
+//     "/image/quotes/quotes_paper_bg6.png",
+//     "/image/quotes/quotes_paper_bg7.png",
+//     "/image/quotes/quotes_paper_bg8.png",
+//   ],
+// } as const;
 
-  // 타입 별 이미지 배열
-  const useImageArr = BG_IMAGE[widgetId];
-  // 인용구 데이터
-  const data = await getApiQuotes();
-  // 이미지 배열 난수
-  const randomIndex = Math.floor(Math.random() * useImageArr.length);
+// // 위젯 아이디 타입 가드
+// const isWidgetId = (id: string): id is keyof typeof BG_IMAGE => id in BG_IMAGE;
 
-  return (
-    <QuotesClient
-      data={data}
-      images={useImageArr}
-      randomIndex={randomIndex}
-      widgetId={widgetId}
-    />
-  );
-}
+// export default async function QuotesPage({
+//   params,
+// }: {
+//   params: { widgetId: string };
+// }) {
+//   // 위젯 아이디
+//   const widgetId = (params.widgetId ?? "").trim();
+
+//   // 화이트 리스트에 걸리지 않는 위젯 아이디는 not found 페이지로 이동
+//   if (!isWidgetId(widgetId)) {
+//     notFound();
+//   }
+
+//   // 타입 별 이미지 배열
+//   const useImageArr = BG_IMAGE[widgetId];
+//   // 인용구 데이터
+//   const data = await getApiQuotes();
+//   // 이미지 배열 난수
+//   const randomIndex = Math.floor(Math.random() * useImageArr.length);
+
+//   return (
+//     <QuotesClient
+//       data={data}
+//       images={useImageArr}
+//       randomIndex={randomIndex}
+//       widgetId={widgetId}
+//     />
+//   );
+// }
