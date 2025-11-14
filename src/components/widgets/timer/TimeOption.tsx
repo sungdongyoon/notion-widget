bd57d22e80ae73ff9e302b12ee457bd7ed9aa424
윤동성 <ehdtjd0906@naver.com>
2025-11-15 01:34:03 +0900

    timeOption - 반응형 ui 개선

--- src/components/widgets/timer/TimeOption.tsx
+++ src/components/widgets/timer/TimeOption.tsx
@@ -133,14 +133,14 @@ export default function TimeOption({
   };
 
   // 섹션 스크롤 함수
-  const scrollToSection = (e: React.MouseEvent<HTMLButtonElement>) => {
-    const id = e.currentTarget.value;
+  // const scrollToSection = (e: React.MouseEvent<HTMLButtonElement>) => {
+  //   const id = e.currentTarget.value;
 
-    document.getElementById(id)?.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
-  };
+  //   document.getElementById(id)?.scrollIntoView({
+  //     behavior: "smooth",
+  //     block: "start",
+  //   });
+  // };
 
   // 시간 세팅
   useEffect(() => {
@@ -169,9 +169,11 @@ export default function TimeOption({
     max-h-[min(70vh,420px)]
     overflow-auto
     flex flex-col gap-4
-    p-4 sm:p-5 sm:pt-0"
+    p-4 sm:p-5 sm:pt-0
+    scroll-pt-16"
+        onOpenAutoFocus={(e) => e.preventDefault()}
       >
-        <div className="w-full sticky pt-3 top-0 flex justify-center items-center flex-wrap">
+        <div className="w-full sticky pt-3 top-0 flex justify-center items-center flex-wrap z-10">
           <Tabs
             onValueChange={(value) => {
               document.getElementById(value)?.scrollIntoView({
@@ -181,27 +183,51 @@ export default function TimeOption({
             }}
           >
             <TabsList>
-              <TabsTrigger value="time">Time</TabsTrigger>
-              <TabsTrigger value="theme">Theme</TabsTrigger>
-              <TabsTrigger value="label">Label</TabsTrigger>
-              <TabsTrigger value="color">Color</TabsTrigger>
+              <TabsTrigger
+                value="time"
+                className="text-[clamp(0.6rem,3vmin,0.8rem)]"
+              >
+                Time
+              </TabsTrigger>
+              <TabsTrigger
+                value="theme"
+                className="text-[clamp(0.6rem,3vmin,0.8rem)]"
+              >
+                Theme
+              </TabsTrigger>
+              <TabsTrigger
+                value="label"
+                className="text-[clamp(0.6rem,3vmin,0.8rem)]"
+              >
+                Label
+              </TabsTrigger>
+              <TabsTrigger
+                value="color"
+                className="text-[clamp(0.6rem,3vmin,0.8rem)]"
+              >
+                Color
+              </TabsTrigger>
             </TabsList>
           </Tabs>
-          {/* <Button size="sm" onClick={handleApply}>
-            Save
-          </Button> */}
         </div>
-        <div className="grid gap-4" id="time">
+        <div className="grid gap-4 scroll-mt-3" id="time">
           <div className="space-y-2">
-            <h4 className="leading-none font-medium">Time Select</h4>
-            <p className="text-muted-foreground text-sm">
+            <h4 className="leading-none font-medium text-[clamp(0.8rem,3vmin,1rem)]">
+              Time Select
+            </h4>
+            <p className="text-muted-foreground text-[clamp(0.6rem,3vmin,0.8rem)]">
               타이머 시간을 설정해주세요.
             </p>
           </div>
           <div className="grid gap-2">
             {activeOption?.includes("hour") && (
               <div className="grid grid-cols-3 items-center gap-4">
-                <Label htmlFor="hour">hour</Label>
+                <Label
+                  htmlFor="hour"
+                  className="text-[clamp(0.6rem,3vmin,0.8rem)]"
+                >
+                  hour
+                </Label>
                 <Input
                   id="hour"
                   type="text"
@@ -209,13 +235,18 @@ export default function TimeOption({
                   value={hour}
                   onChange={onHourChange}
                   onBlur={() => setHour(onBlurClamp(hour, 23))}
-                  className="col-span-2 h-8"
+                  className="col-span-2 h-[clamp(1.5rem,5vmin,2rem)] text-[clamp(0.6rem,3vmin,0.8rem)]"
                 />
               </div>
             )}
             {activeOption?.includes("minute") && (
               <div className="grid grid-cols-3 items-center gap-4">
-                <Label htmlFor="minute">minute</Label>
+                <Label
+                  htmlFor="minute"
+                  className="text-[clamp(0.6rem,3vmin,0.8rem)]"
+                >
+                  minute
+                </Label>
                 <Input
                   id="minute"
                   type="text"
@@ -223,13 +254,18 @@ export default function TimeOption({
                   value={minute}
                   onChange={onMinuteChange}
                   onBlur={() => setMinute(onBlurClamp(minute, 59))}
-                  className="col-span-2 h-8"
+                  className="col-span-2 h-[clamp(1.5rem,5vmin,2rem)] text-[clamp(0.6rem,3vmin,0.8rem)]"
                 />
               </div>
             )}
             {activeOption?.includes("second") && (
               <div className="grid grid-cols-3 items-center gap-4">
-                <Label htmlFor="second">second</Label>
+                <Label
+                  htmlFor="second"
+                  className="text-[clamp(0.6rem,3vmin,0.8rem)]"
+                >
+                  second
+                </Label>
                 <Input
                   id="second"
                   type="text"
@@ -237,7 +273,7 @@ export default function TimeOption({
                   value={second}
                   onChange={onSecondChange}
                   onBlur={() => setSecond(onBlurClamp(second, 59))}
-                  className="col-span-2 h-8"
+                  className="col-span-2 h-[clamp(1.5rem,5vmin,2rem)] text-[clamp(0.6rem,3vmin,0.8rem)]"
                 />
               </div>
             )}
@@ -246,22 +282,24 @@ export default function TimeOption({
 
         <div className="grid gap-4 mt-7" id="theme">
           <div className="space-y-2">
-            <h4 className="leading-none font-medium">Theme</h4>
-            <p className="text-muted-foreground text-sm">
+            <h4 className="leading-none font-medium text-[clamp(0.8rem,3vmin,1rem)]">
+              Theme
+            </h4>
+            <p className="text-muted-foreground text-[clamp(0.6rem,3vmin,0.8rem)]">
               타이머 테마를 설정해주세요.
             </p>
           </div>
-          <div className="flex flex-col gap-1">
+          <div className="flex gap-1">
             <Button
               variant="outline"
-              className="bg-white text-black"
+              className="bg-white text-black text-[clamp(0.6rem,3vmin,0.8rem)] h-[clamp(1.7rem,5vmin,2rem)]"
               onClick={() => setTheme("light")}
             >
               Light
             </Button>
             <Button
               variant="outline"
-              className="bg-black text-white"
+              className="bg-black text-white text-[clamp(0.6rem,3vmin,0.8rem)] h-[clamp(1.7rem,5vmin,2rem)]"
               onClick={() => setTheme("dark")}
             >
               Dark
@@ -272,8 +310,10 @@ export default function TimeOption({
         {activeOption?.includes("label") && (
           <div className="grid gap-4 mt-7" id="label">
             <div className="space-y-2">
-              <h4 className="leading-none font-medium">Label</h4>
-              <p className="text-muted-foreground text-sm">
+              <h4 className="leading-none font-medium text-[clamp(0.8rem,3vmin,1rem)]">
+                Label
+              </h4>
+              <p className="text-muted-foreground text-[clamp(0.6rem,3vmin,0.8rem)]">
                 타이머에 표시할 라벨을 입력해주세요.
               </p>
             </div>
@@ -284,7 +324,7 @@ export default function TimeOption({
                 placeholder="예) study 📝"
                 value={timerLabel.label}
                 onChange={onLabelChange}
-                className={`col-span-2 h-8 ${
+                className={`col-span-2 h-[clamp(1.5rem,5vmin,2rem)] text-[clamp(0.6rem,3vmin,0.8rem)] ${
                   overLimit ? "timer_label_input border-red-500" : ""
                 }`}
               />
@@ -294,7 +334,7 @@ export default function TimeOption({
                   style={{ width: `${labelPercent}%` }}
                 />
               </div>
-              <p className="text-xs text-gray-500 mt-1 text-right">
+              <p className="text-[clamp(0.6rem,3vmin,0.8rem)] text-gray-500 mt-1 text-right">
                 {timerLabel.length} / {LABEL_MAX_LENGTH}
               </p>
             </div>
@@ -304,8 +344,10 @@ export default function TimeOption({
         {activeOption?.includes("color") && (
           <div className="grid gap-4 mt-7" id="color">
             <div className="space-y-2">
-              <h4 className="leading-none font-medium">Color</h4>
-              <p className="text-muted-foreground text-sm">
+              <h4 className="leading-none font-medium text-[clamp(0.8rem,3vmin,1rem)]">
+                Color
+              </h4>
+              <p className="text-muted-foreground text-[clamp(0.6rem,3vmin,0.8rem)]">
                 타이머의 메인 색상을 선택해주세요.
               </p>
             </div>
@@ -330,6 +372,14 @@ export default function TimeOption({
             </div>
           </div>
         )}
+        <Button
+          size="sm"
+          variant="destructive"
+          className="fixed bottom-2 right-5 bg-red-500 h-[clamp(1.8rem,5vmin,2rem)] px-[clamp(1rem,3vmin,1.5rem)] text-[clamp(0.6rem,3vmin,0.8rem)]"
+          onClick={handleApply}
+        >
+          저장
+        </Button>
       </PopoverContent>
     </Popover>
   );
