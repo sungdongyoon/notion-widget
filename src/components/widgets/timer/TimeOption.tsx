8758701e85a4e41dfc387ba74f0c437ec09e2e3b
윤동성 <ehdtjd0906@naver.com>
2026-01-29 20:01:21 +0900

    TimeOption.tsx - 모바일 환경 크기 테스트

--- src/components/widgets/timer/TimeOption.tsx
+++ src/components/widgets/timer/TimeOption.tsx
@@ -189,10 +189,11 @@ export default function TimeOption({
       <DialogContent
         className="w-[clamp(10rem,95vw,22rem)]
     aspect-square
-    overflow-scroll
+    overflow-auto
     flex flex-col items-end gap-3
     p-[clamp(0.6rem,3vmin,1rem)]
     scroll-pt-16
+    max-h-[200px]
     "
         onOpenAutoFocus={(e) => e.preventDefault()}
         showOverlay={false}
