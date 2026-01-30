42959deee995534b30b0497d0198abaa6dfbc728
윤동성 <ehdtjd0906@naver.com>
2026-01-30 21:08:39 +0900

    TimeOption.tsx - 모바일 환경에서 dialog portal 제거 및 크기 개선

--- src/components/widgets/timer/TimeOption.tsx
+++ src/components/widgets/timer/TimeOption.tsx
@@ -196,11 +196,12 @@ export default function TimeOption({
     flex flex-col items-end gap-3
     p-[clamp(0.6rem,3vmin,1rem)]
     scroll-pt-16
-    ${isMobile ? "max-h-[40%]" : "aspect-square"}
+    ${isMobile ? "max-h-[50%] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" : "aspect-square"}
     `}
         onOpenAutoFocus={(e) => e.preventDefault()}
         showOverlay={false}
         showClose={false}
+        isPortal={isMobile ? false : true}
       >
         <VisuallyHidden>
           <DialogHeader>
