d2c9aca9d2749c699520200880ceb1ca27a724ca
윤동성 <ehdtjd0906@naver.com>
2026-02-03 18:42:11 +0900

    Calendar01.tsx - shadcn ui Calendar 컴포넌트 커스텀 세팅

--- src/components/widgets/calendar/Calendar01.tsx
+++ src/components/widgets/calendar/Calendar01.tsx
@@ -1,22 +1,36 @@
 "use client";
 
-import { Calendar } from "@/components/ui/calendar";
+import { Calendar, CalendarDayButton } from "@/components/ui/calendar";
 import React, { useState } from "react";
+import { CalendarDay } from "react-day-picker";
 
 const Calendar01 = () => {
   const [date, setDate] = useState<Date | any>(new Date());
 
   console.log("date", date);
   return (
-    <div className="widget_container bg-red-100" data-variant="calendar01">
-      <div className="max-w-[100dvh] w-full aspect-square">
-        <Calendar
-          mode="single"
-          selected={date}
-          onSelect={setDate}
-          className="w-full h-full"
-        />
-      </div>
+    <div className="widget_container" data-variant="calendar01">
+      <Calendar
+        mode="single"
+        selected={date}
+        onSelect={setDate}
+        className="w-full h-full bg-blue-50 max-w-[100dvh] aspect-square"
+        classNames={{
+          months: "calendar_months",
+          month: "calendar_month",
+          month_caption: "calendar_month_caption",
+          nav: "calendar_nav",
+          button_previous: "calendar_button_previous",
+          button_next: "calendar_button_next",
+          caption_label:
+            "calendar_caption_label text-[clamp(0.6rem,3vmin,1rem)]",
+          weekdays: "calendar_weekdays",
+          weekday: "calendar_weekday text-[clamp(0.6rem,3vmin,1rem)]",
+          week: "calendar_week",
+          day: "calendar_day bg-red-100 text-[clamp(0.4rem,3vmin,1rem)]",
+          today: "calendar_today bg-blue-100",
+        }}
+      />
     </div>
   );
 };
